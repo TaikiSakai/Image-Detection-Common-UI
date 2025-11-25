f2efd89ccd790c2c0b0055f186b9870065b65eb8
TaikiSakai <taiki.sakai@arsaga.jp>
2025-11-25 17:57:45 +0900

    tailwindの設定を修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,30 @@ export default {
     "./index.html",
   ],
   theme: {
+    colors: {
+      blue: {
+        g: '#0000FF',
+        5: '#006BDD',
+        4: '#0093E9',
+        3: '#00AEEF',
+        2: '#66CEF5',
+        1: '#CCEFFC',
+      },
+      red: {
+        5: '#FF0000',
+        1: '#FFE2E2',
+      },
+      black: {
+        g: '#000000',
+        5: '#4C4C4C',
+        4: '#747474',
+        3: '#A0A0A0',
+        2: '#BABABA',
+        1: '#fbfafaff',
+        0: '#FFFFFF',
+      },
+
+    },
     extend: {
       animation: {
         'spin-slow': 'spin 20s linear infinite',
