4bc93865b1918e28de9d2050dfddbf3f756cfb51
TaikiSakai <taiki.sakai@arsaga.jp>
2025-11-25 17:58:47 +0900

    feat: サイドバーコンポーネントを追加

---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,16 @@
+import type { ReactNode } from 'react';
+
+type SidebarProps = {
+  children?: ReactNode;
+};
+
+export const Sidebar = ({ children }: SidebarProps) => {
+  return (
+    <div className="w-64 h-full bg-white rounded-lg shadow-elevation overflow-y-auto bg-black-0">
+      <div className="p-4">
+        <h2 className="text-lg font-bold mb-4">読み込んだ画像</h2>
+        <div className="flex flex-col gap-2">{children}</div>
+      </div>
+    </div>
+  );
+};
