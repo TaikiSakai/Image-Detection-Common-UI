3d65b8c90680e402f5f66a0241c35c4f2d612376
TaikiSakai <taiki.sakai@arsaga.jp>
2025-11-26 15:27:39 +0900

    fix: CSSを修正修正

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,9 @@
 import reactLogo from '@/assets/react.svg';
+import { Button } from '@/components/common/button';
 
 export const Header = () => {
   return (
-    <header className="mb-2 py-1 shadow-elevation bg-black-0">
+    <header className="mb-1 py-1 shadow-md bg-black-0">
       <div className="flex items-center gap-10 bg-white px-6 py-3">
         <div className="flex items-center gap-4">
           <img
@@ -12,7 +13,7 @@ export const Header = () => {
           />
           <h1 className="text-2xl m-0">画像認識共通UI</h1>
         </div>
-        <button>仮置きのボタン</button>
+        <Button className="hover:bg-black-2 shadow-md">仮おきのボタン</Button>
       </div>
     </header>
   );
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -6,7 +6,7 @@ type SidebarProps = {
 
 export const Sidebar = ({ children }: SidebarProps) => {
   return (
-    <div className="w-64 h-full bg-white shadow-elevation overflow-y-auto bg-black-0">
+    <div className="w-64 h-full bg-white shadow-md overflow-y-auto bg-black-0">
       <div className="p-4">
         <div className="flex justify-center mb-4">
           <Button className='hover:bg-black-2 shadow-elevation' onClick={() => console.log('clicked!')}>Load Images</Button>
